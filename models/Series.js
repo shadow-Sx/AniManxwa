39c2809520b83009670cf4133499382fce6a5384
shadow-Sx <shadowsoya8@gmail.com>
2026-09-21 22:40:39 +0500

    Update Series.js

--- models/Series.js
+++ models/Series.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const seriesSchema = new mongoose.Schema(
   {
     title: { type: String, required: true },
+    slug: { type: String, unique: true, sparse: true },
     description: String,
     author: String,
     studio: String,
